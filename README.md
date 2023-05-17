1f284d30de4db599c0a08a6fbb26eb114457d2f0
lucasheck <lucasheck23@gmail.com>
2023-05-17 18:41:15 -0400

    1 - Installing prismaDB 2 - Installing Dependencies Packages 3 - Creating the first route

---
+++ README.md
@@ -0,0 +1,36 @@
+# Spacetime
+
+Spacetime - Server
+
+Training Node Project: Timeline project
+
+##
+
+Objective:
+
+This project was created for training and improve development skills only.
+
+##
+
+Type npm start to build and run
+
+##
+
+Technologies used:
+
+-   Node.js
+-   Typescript
+-   Prisma
+-   Fastify Framework
+
+##
+
+DONE:
+
+-
+
+TO DO:
+
+-   Everything
+
+##
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,19 @@
-console.log("teste");
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
-console.log("teste");
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running on http://localhost:3333')
+  })
